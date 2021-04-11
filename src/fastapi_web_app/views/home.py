abda7cb479d31cc65d58496fcad0c55510884883
Derek Wan <d.wan@icloud.com>
2021-04-12 00:14:52 +0800

    Use the external index.pt

--- src/fastapi_web_app/views/home.py
+++ src/fastapi_web_app/views/home.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from fastapi import APIRouter
 from fastapi_chameleon import template
 
@@ -7,8 +9,14 @@ router = APIRouter()
 
 @router.get("/")
 @template()
-def index() -> dict[str, str]:
-    return {"user_name": "derek"}
+def index() -> dict[str, Any]:
+    return {
+        "user_name": "derek",
+        "package_count": 274000,
+        "release_count": 2234847,
+        "user_count": 73874,
+        "packages": [{"id": "fastapi", "summary": "What you want to master"}],
+    }
 
 
 @router.get("/about")
