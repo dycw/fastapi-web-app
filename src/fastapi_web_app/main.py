bd9bc76094e188d45b66e28f6eadcab681e5a933
Derek Wan <d.wan@icloud.com>
2021-04-11 21:49:00 +0800

    Use fastapi-chameleon template (#4)
    * Put in __name__=="__main__"
    
    * Return an HTMLResponse
    
    * Add chameleon
    
    * Add fastapi-chameleon
    
    * Remove __pycache__/
    
    * Use index.html template

--- src/fastapi_web_app/main.py
+++ src/fastapi_web_app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+from fastapi_chameleon import global_init
+from fastapi_chameleon import template
 from uvicorn import run
 
 
 app = FastAPI()
+global_init("templates")
 
 
 @app.get("/")
+@template(template_file="index.html")
 def index() -> dict[str, str]:
-    return {"message": "Hello world"}
+    return {"user_name": "derek"}
 
 
-run(app)
+if __name__ == "__main__":
+    run(app)
