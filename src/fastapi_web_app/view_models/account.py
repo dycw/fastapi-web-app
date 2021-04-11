8a72df6e8602cd61efd8dfddbf20634797d07ce9
Derek Wan <d.wan@icloud.com>
2021-04-12 01:04:16 +0800

    Have all functions use view models (#13)

---
+++ src/fastapi_web_app/view_models/account.py
@@ -0,0 +1,13 @@
+from fastapi_web_app.view_models.base import ViewModelBase
+
+
+class AccountViewModel(ViewModelBase):
+    pass
+
+
+class RegisterViewModel(ViewModelBase):
+    pass
+
+
+class LoginViewModel(ViewModelBase):
+    pass
--- src/fastapi_web_app/views/account.py
+++ src/fastapi_web_app/views/account.py
@@ -1,22 +1,27 @@
 from fastapi import APIRouter
+from starlette.requests import Request
+
+from fastapi_web_app.view_models.account import AccountViewModel
+from fastapi_web_app.view_models.account import LoginViewModel
+from fastapi_web_app.view_models.account import RegisterViewModel
 
 
 router = APIRouter()
 
 
 @router.get("/account")
-def index() -> dict[str, str]:
-    return {}
+def index(request: Request) -> dict[str, str]:
+    return AccountViewModel(request).to_dict()
 
 
 @router.get("/account/register")
-def register() -> dict[str, str]:
-    return {}
+def register(request: Request) -> dict[str, str]:
+    return RegisterViewModel(request).to_dict()
 
 
 @router.get("/account/login")
-def login() -> dict[str, str]:
-    return {}
+def login(request: Request) -> dict[str, str]:
+    return LoginViewModel(request).to_dict()
 
 
 @router.get("/account/logout")
