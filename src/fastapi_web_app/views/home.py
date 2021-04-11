770d294e720a4ad1b3b87540fda71efb883acfba
Derek Wan <d.wan@icloud.com>
2021-04-12 01:02:18 +0800

    Have all functions use view models

--- src/fastapi_web_app/views/home.py
+++ src/fastapi_web_app/views/home.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter
 from fastapi_chameleon import template
 from starlette.requests import Request
 
-from fastapi_web_app.view_models.home.index import IndexViewModel
+from fastapi_web_app.view_models.base import ViewModelBase
+from fastapi_web_app.view_models.index import IndexViewModel
 
 
 router = APIRouter()
@@ -13,11 +14,10 @@ router = APIRouter()
 @router.get("/")
 @template()
 def index(request: Request) -> dict[str, Any]:
-    vm = IndexViewModel(request)
-    return vm.to_dict()
+    return IndexViewModel(request).to_dict()
 
 
 @router.get("/about")
 @template()
-def about() -> dict[str, str]:
-    return {}
+def about(request: Request) -> dict[str, str]:
+    return ViewModelBase(request).to_dict()
