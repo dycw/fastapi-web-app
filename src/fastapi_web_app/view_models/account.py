f1355e308c66bf99a1c9160851739ecd2894a66f
Derek Wan <d.wan@icloud.com>
2021-04-12 09:47:57 +0800

    Implement register

--- src/fastapi_web_app/view_models/account.py
+++ src/fastapi_web_app/view_models/account.py
@@ -1,12 +1,38 @@
+from typing import Optional
+
+from starlette.requests import Request
+
+from fastapi_web_app.data.user import User
 from fastapi_web_app.view_models.base import ViewModelBase
 
 
 class AccountViewModel(ViewModelBase):
-    pass
+    def __init__(self, request: Request) -> None:
+        super().__init__(request)
+        self.user = User("Derek", "d.wan@icloud.com", "hpassword")
 
 
 class RegisterViewModel(ViewModelBase):
-    pass
+    def __init__(self, request: Request) -> None:
+        super().__init__(request)
+        self.name: Optional[str] = None
+        self.password: Optional[str] = None
+        self.email: Optional[str] = None
+
+    async def load(self) -> None:
+        form = await self.request.form()
+        self.name = form.get("name")
+        self.password = form.get("password")
+        self.email = form.get("email")
+
+        if not self.name or not self.name.strip():
+            self.error = "Your name is required."
+        elif not self.email or not self.email.strip():
+            self.error = "Your email is required."
+        elif not self.password or len(self.password) < 5:
+            self.error = (
+                "Your password is required and must be at least 5 chars"
+            )
 
 
 class LoginViewModel(ViewModelBase):
--- src/fastapi_web_app/views/account.py
+++ src/fastapi_web_app/views/account.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter
-from starlette.requests import Request
+from typing import Union
 
+from fastapi import APIRouter
+from fastapi_chameleon import template
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.status import HTTP_302_FOUND
+
+from fastapi_web_app.services.users import create_account
 from fastapi_web_app.view_models.account import AccountViewModel
 from fastapi_web_app.view_models.account import LoginViewModel
 from fastapi_web_app.view_models.account import RegisterViewModel
@@ -10,20 +16,38 @@ router = APIRouter()
 
 
 @router.get("/account")
+@template()
 def index(request: Request) -> dict[str, str]:
     return AccountViewModel(request).to_dict()
 
 
 @router.get("/account/register")
+@template()
 def register(request: Request) -> dict[str, str]:
     return RegisterViewModel(request).to_dict()
 
 
+@router.post("/account/register")
+@template()
+async def register_p(
+    request: Request,
+) -> Union[dict[str, str], RedirectResponse]:
+    vm = RegisterViewModel(request)
+    await vm.load()
+    if vm.error:
+        return vm.to_dict()
+    else:
+        create_account(vm.name, vm.email, vm.password)
+        return RedirectResponse("/account", status_code=HTTP_302_FOUND)
+
+
 @router.get("/account/login")
+@template()
 def login(request: Request) -> dict[str, str]:
     return LoginViewModel(request).to_dict()
 
 
 @router.get("/account/logout")
+@template()
 def logout() -> dict[str, str]:
     return {}
