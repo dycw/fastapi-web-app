46215ef38e03713f1751158715fab54585c7bcb7
Derek Wan <d.wan@icloud.com>
2021-04-12 11:25:32 +0800

    Sync end of chapter (#18)

--- src/fastapi_web_app/services/users.py
+++ src/fastapi_web_app/services/users.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi_web_app.data.user import User
 
 
@@ -7,3 +9,10 @@ def user_count() -> int:
 
 def create_account(name: str, email: str, password: str) -> User:
     return User(name, email, password)
+
+
+def login_user(email: str, password: str) -> Optional[User]:
+    if password == "abc":  # noqa: S105
+        return User("derek", email, password)
+    else:
+        return None
