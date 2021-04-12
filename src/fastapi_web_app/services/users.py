342d2e39550da79d1835445c89697cac95d41ca9
Derek Wan <d.wan@icloud.com>
2021-04-12 17:15:55 +0800

    Add passlib for verifying

--- src/fastapi_web_app/services/users.py
+++ src/fastapi_web_app/services/users.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from passlib.handlers.sha2_crypt import sha512_crypt
+
 from fastapi_web_app.data.db_session import create_session
 from fastapi_web_app.data.user import User
 
@@ -12,10 +14,14 @@ def user_count() -> int:
         session.close()
 
 
-def create_account(name: str, email: str, password: str) -> User:  # noqa: U100
+def create_account(name: str, email: str, password: str) -> User:
     session = create_session()
     try:
-        user = User(name=name, email=email, hash_password="TBD")  # noqa: S106
+        user = User(
+            name=name,
+            email=email,
+            hash_password=sha512_crypt.hash(password, rounds=172434),
+        )
         session.add(user)
         session.commit()
         return user
@@ -23,14 +29,17 @@ def create_account(name: str, email: str, password: str) -> User:  # noqa: U100
         session.close()
 
 
-def login_user(email: str, password: str) -> Optional[User]:  # noqa: U100
+def login_user(email: str, password: str) -> Optional[User]:
     session = create_session()
     try:
         user = session.query(User).filter(User.email == email).first()
         if not user:
             return user
         else:
-            return user
+            if sha512_crypt.verify(password, user.hash_password):
+                return user
+            else:
+                return None
     finally:
         session.close()
 
