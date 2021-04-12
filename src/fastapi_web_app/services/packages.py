918847f734a7dabf7cfb12560f3c17c290a13748
Derek Wan <d.wan@icloud.com>
2021-04-12 15:59:04 +0800

    Query database for the services (#22)
    * Query database
    
    * Populate latest packages too
    
    * Fix type hint

--- src/fastapi_web_app/services/packages.py
+++ src/fastapi_web_app/services/packages.py
@@ -1,20 +1,44 @@
 import datetime as dt
 from typing import Optional
+from typing import cast
 
+from sqlalchemy import Column
+from sqlalchemy.orm import joinedload
+
+from fastapi_web_app.data.db_session import create_session
 from fastapi_web_app.data.package import Package
 from fastapi_web_app.data.release import Release
 
 
 def package_count() -> int:
-    return 274000
+    session = create_session()
+    try:
+        return session.query(Package).count()
+    finally:
+        session.close()
 
 
 def release_count() -> int:
-    return 2234847
+    session = create_session()
+    try:
+        return session.query(Release).count()
+    finally:
+        session.close()
 
 
-def latest_packages(*, limit: int = 5) -> list[dict[str, str]]:
-    return [{"id": "fastapi", "summary": "What you want to master"}][:limit]
+def latest_packages(*, limit: int = 5) -> list[Package]:
+    session = create_session()
+    try:
+        releases = (
+            session.query(Release)
+            .options(joinedload(Release.package))
+            .order_by(cast(Column, Release.created_date).desc())
+            .limit(limit)
+            .all()
+        )
+        return [r.package for r in releases]
+    finally:
+        session.close()
 
 
 def get_package_by_id(package_name: str) -> Optional[Package]:
