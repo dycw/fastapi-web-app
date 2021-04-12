24eafa19190574e14cecfce7fc95b2a3bcc44963
Derek Wan <d.wan@icloud.com>
2021-04-12 16:25:34 +0800

    Moer queries

--- src/fastapi_web_app/services/users.py
+++ src/fastapi_web_app/services/users.py
@@ -12,12 +12,40 @@ def user_count() -> int:
         session.close()
 
 
-def create_account(name: str, email: str, password: str) -> User:
-    return User(name, email, password)
+def create_account(name: str, email: str, password: str) -> User:  # noqa: U100
+    session = create_session()
+    try:
+        user = User(name=name, email=email, hash_password="TBD")  # noqa: S106
+        session.add(user)
+        session.commit()
+        return user
+    finally:
+        session.close()
 
 
-def login_user(email: str, password: str) -> Optional[User]:
-    if password == "abc":  # noqa: S105
-        return User("derek", email, password)
-    else:
-        return None
+def login_user(email: str, password: str) -> Optional[User]:  # noqa: U100
+    session = create_session()
+    try:
+        user = session.query(User).filter(User.email == email).first()
+        if not user:
+            return user
+        else:
+            return user
+    finally:
+        session.close()
+
+
+def get_user_by_id(user_id: int) -> Optional[User]:
+    session = create_session()
+    try:
+        return session.query(User).filter(User.id == user_id).first()
+    finally:
+        session.close()
+
+
+def get_user_by_email(email: str) -> Optional[User]:
+    session = create_session()
+    try:
+        return session.query(User).filter(User.email == email).first()
+    finally:
+        session.close()
--- src/fastapi_web_app/view_models/base.py
+++ src/fastapi_web_app/view_models/base.py
@@ -12,8 +12,8 @@ class ViewModelBase:
     def __init__(self, request: Request) -> None:
         self.request = request
         self.error: Optional[str] = None
-        self.user_id: Optional[str] = None
-        self.is_logged_in = get_user_id_via_auth_cookie(self.request)
+        self.user_id: Optional[int] = get_user_id_via_auth_cookie(self.request)
+        self.is_logged_in = self.user_id is not None
 
     def to_dict(self) -> dict[str, Any]:
         return self.__dict__
