72798ef4ef66c33da182a0aee01dd544b447c335
Derek Wan <d.wan@icloud.com>
2021-04-12 18:08:18 +0800

    Add async engine and make some views/funcs async (#26)
    * Change IndexViewModel to be async
    
    * Make more functions async

--- src/fastapi_web_app/services/users.py
+++ src/fastapi_web_app/services/users.py
@@ -1,17 +1,19 @@
 from typing import Optional
 
 from passlib.handlers.sha2_crypt import sha512_crypt
+from sqlalchemy import func
+from sqlalchemy import select
 
+from fastapi_web_app.data.db_session import create_async_session
 from fastapi_web_app.data.db_session import create_session
 from fastapi_web_app.data.user import User
 
 
-def user_count() -> int:
-    session = create_session()
-    try:
-        return session.query(User).count()
-    finally:
-        session.close()
+async def user_count() -> int:
+    sel = select(func.count(User.id))
+    async with create_async_session() as session:
+        result = await session.execute(sel)
+        return result.scalar()
 
 
 def create_account(name: str, email: str, password: str) -> User:
@@ -44,17 +46,13 @@ def login_user(email: str, password: str) -> Optional[User]:
         session.close()
 
 
-def get_user_by_id(user_id: int) -> Optional[User]:
-    session = create_session()
-    try:
-        return session.query(User).filter(User.id == user_id).first()
-    finally:
-        session.close()
+async def get_user_by_id(user_id: int) -> Optional[User]:
+    sel = select(User).filter(User.id == user_id)
+    async with create_async_session() as session:
+        return session.execute(sel).scalar_one_or_none()
 
 
-def get_user_by_email(email: str) -> Optional[User]:
-    session = create_session()
-    try:
-        return session.query(User).filter(User.email == email).first()
-    finally:
-        session.close()
+async def get_user_by_email(email: str) -> Optional[User]:
+    sel = select(User).filter(User.email == email)
+    async with create_async_session() as session:
+        return session.execute(sel).scalar_one_or_none()
--- src/fastapi_web_app/view_models/index.py
+++ src/fastapi_web_app/view_models/index.py
@@ -11,7 +11,13 @@ from fastapi_web_app.view_models.base import ViewModelBase
 class IndexViewModel(ViewModelBase):
     def __init__(self, request: Request) -> None:
         super().__init__(request)
+        self.release_count: int = 0
+        self.user_count: int = 0
+        self.package_count: int = 0
+        self.packages: list[Package] = []
+
+    async def load(self) -> None:
+        self.release_count = release_count()
+        self.user_count: int = await user_count()
         self.package_count: int = package_count()
-        self.release_count: int = release_count()
-        self.user_count: int = user_count()
         self.packages: list[Package] = latest_packages(limit=5)
