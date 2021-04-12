9f995993b62d753ac6719f62a775fdba36ffd2ef
Derek Wan <d.wan@icloud.com>
2021-04-12 09:24:03 +0800

    Add User

---
+++ src/fastapi_web_app/data/user.py
@@ -0,0 +1,12 @@
+import datetime as dt
+from typing import Optional
+
+
+class User:
+    def __init__(self, name: str, email: str, hashed_password: str) -> None:
+        self.name = name
+        self.email = email
+        self.hashed_password = hashed_password
+        self.created_date = None
+        self.profile_image_url = ""
+        self.last_login: Optional[dt.datetime] = None
