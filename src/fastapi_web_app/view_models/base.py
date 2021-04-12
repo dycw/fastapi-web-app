2aabfa0bb30872f1f53a048672e7c422eedb91ba
Derek Wan <d.wan@icloud.com>
2021-04-12 10:13:14 +0800

    Add cookie auth

--- src/fastapi_web_app/view_models/base.py
+++ src/fastapi_web_app/view_models/base.py
@@ -3,13 +3,17 @@ from typing import Optional
 
 from starlette.requests import Request
 
+from fastapi_web_app.infrastructure.cookie_auth import (
+    get_user_id_from_auth_cookie,
+)
+
 
 class ViewModelBase:
     def __init__(self, request: Request) -> None:
         self.request = request
         self.error: Optional[str] = None
         self.user_id: Optional[str] = None
-        self.is_logged_in = False
+        self.is_logged_in = get_user_id_from_auth_cookie(self.request)
 
     def to_dict(self) -> dict[str, Any]:
         return self.__dict__
--- src/fastapi_web_app/views/account.py
+++ src/fastapi_web_app/views/account.py
@@ -6,6 +6,7 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 from starlette.status import HTTP_302_FOUND
 
+from fastapi_web_app.infrastructure.cookie_auth import set_auth
 from fastapi_web_app.services.users import create_account
 from fastapi_web_app.view_models.account import AccountViewModel
 from fastapi_web_app.view_models.account import LoginViewModel
@@ -37,8 +38,10 @@ async def register_p(
     if vm.error or vm.name is None or vm.email is None or vm.password is None:
         return vm.to_dict()
     else:
-        create_account(vm.name, vm.email, vm.password)
-        return RedirectResponse("/account", status_code=HTTP_302_FOUND)
+        user = create_account(vm.name, vm.email, vm.password)
+        response = RedirectResponse("/account", status_code=HTTP_302_FOUND)
+        set_auth(response, user.id)
+        return response
 
 
 @router.get("/account/login")
