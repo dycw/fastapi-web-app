26348599f28525af3df2ed7ef8e43b98a5c73f26
Derek Wan <d.wan@icloud.com>
2021-04-12 14:49:32 +0800

    Fix type hint

--- src/fastapi_web_app/view_models/index.py
+++ src/fastapi_web_app/view_models/index.py
@@ -1,5 +1,6 @@
 from starlette.requests import Request
 
+from fastapi_web_app.data.package import Package
 from fastapi_web_app.services.packages import latest_packages
 from fastapi_web_app.services.packages import package_count
 from fastapi_web_app.services.packages import release_count
@@ -13,4 +14,4 @@ class IndexViewModel(ViewModelBase):
         self.package_count: int = package_count()
         self.release_count: int = release_count()
         self.user_count: int = user_count()
-        self.packages: list[dict[str, str]] = latest_packages(limit=5)
+        self.packages: list[Package] = latest_packages(limit=5)
