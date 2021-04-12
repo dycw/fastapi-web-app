06fbd6f40c422776a3c39727154af6d0b1564150
Derek Wan <d.wan@icloud.com>
2021-04-12 09:51:41 +0800

    Define register post (#15)
    * Add User
    
    * Add templates/account
    
    * Implement register
    
    * Help pyright

--- src/fastapi_web_app/services/users.py
+++ src/fastapi_web_app/services/users.py
@@ -1,2 +1,9 @@
+from fastapi_web_app.data.user import User
+
+
 def user_count() -> int:
     return 73874
+
+
+def create_account(name: str, email: str, password: str) -> User:
+    return User(name, email, password)
