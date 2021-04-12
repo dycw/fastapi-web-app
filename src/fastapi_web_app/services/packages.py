72b5a934dd39d5a29bd9af100cca8f6bdd534e1a
Derek Wan <d.wan@icloud.com>
2021-04-12 14:48:41 +0800

    Populate latest packages too

--- src/fastapi_web_app/services/packages.py
+++ src/fastapi_web_app/services/packages.py
@@ -1,5 +1,9 @@
 import datetime as dt
 from typing import Optional
+from typing import cast
+
+from sqlalchemy import Column
+from sqlalchemy.orm import joinedload
 
 from fastapi_web_app.data.db_session import create_session
 from fastapi_web_app.data.package import Package
@@ -22,8 +26,19 @@ def release_count() -> int:
         session.close()
 
 
-def latest_packages(*, limit: int = 5) -> list[dict[str, str]]:
-    return [{"id": "fastapi", "summary": "What you want to master"}][:limit]
+def latest_packages(*, limit: int = 5) -> list[Package]:
+    session = create_session()
+    try:
+        releases = (
+            session.query(Release)
+            .options(joinedload(Release.package))
+            .order_by(cast(Column, Release.created_date).desc())
+            .limit(limit)
+            .all()
+        )
+        return [r.package for r in releases]
+    finally:
+        session.close()
 
 
 def get_package_by_id(package_name: str) -> Optional[Package]:
