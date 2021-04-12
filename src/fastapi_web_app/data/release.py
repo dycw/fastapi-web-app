04a60f403edc609efe3a7cd3d305f4d2e3b12453
Derek Wan <d.wan@icloud.com>
2021-04-12 13:38:49 +0800

    Make models actual sqlalchemy classes

--- src/fastapi_web_app/data/release.py
+++ src/fastapi_web_app/data/release.py
@@ -1,8 +1,33 @@
 import datetime as dt
 
+from sqlalchemy import BigInteger
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import relation
 
-class Release:
-    def __init__(self, version: str, latest_release: dt.datetime) -> None:
-        self.version = version
-        self.latest_release = latest_release
-        self.created_date = dt.datetime.now()
+from fastapi_web_app.data.modelbase import SqlAlchemyBase
+
+
+class Release(SqlAlchemyBase):
+    __tablename__ = "releases"
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    major_ver: int = Column(BigInteger, index=True)
+    minor_ver: int = Column(BigInteger, index=True)
+    build_ver: int = Column(BigInteger, index=True)
+    created_date: dt.datetime = Column(
+        DateTime, default=dt.datetime.now, index=True
+    )
+    comment: str = Column(String)
+    url: str = Column(String)
+    size: int = Column(BigInteger)
+
+    # package relationship
+    package_id: str = Column(String, ForeignKey("packages.id"))
+    package = relation("Package")
+
+    @property
+    def version_text(self) -> str:
+        return f"{self.major_ver}.{self.minor_ver}.{self.build_ver}"
